b5fb8b3e7fdad7e1af993e5e385053b430d34d93
trueto <18868150826@163.com>
2016-08-13 10:44:03 +0800

    InfoApp v1.0.0

---
+++ InfoAPP/js/app.js
@@ -0,0 +1,96 @@
+/**
+ * 演示程序当前的 “注册/登录” 等操作，是基于 “本地存储” 完成的
+ * 当您要参考这个演示程序进行相关 app 的开发时，
+ * 请注意将相关方法调整成 “基于服务端Service” 的实现。
+ **/
+(function($, owner) {
+	owner.saveUser=function (data) {
+		$.ajax({
+			type:"POST",
+			url:posturl.login,
+			dataType:'json',
+			data:{
+				sex:data.sex,
+				name:data.name,
+				avatar:data.avatar,
+				location:data.location
+			},
+			success:function () {
+				mui.toast("用户信息保存成功！");
+			},
+			error:function () {
+				mui.toast("请检查网络连接！");
+			}
+		});
+	},
+	owner.createState = function(data, callback) {
+		var state = owner.getState();
+		var sex=data.gender;
+		var ulocation=data.province+' '+data.city;
+		if (sex=='m') {
+			sex='男';
+		}
+		state.token=1;
+		state.sex=sex||(data.sex==1?"男":"女");
+		state.name = data.nickname||data.screen_name;
+		state.avatar = data.figureurl_qq_2||data.headimgurl||data.avatar_large;
+		state.location=data.location||ulocation;
+		owner.setState(state);
+		owner.saveUser(state);
+		return callback();
+	};
+	/**
+	 * 获取当前状态
+	 **/
+	owner.getState = function() {
+		var stateText = localStorage.getItem('$state') || "{}";
+		return JSON.parse(stateText);
+	};
+
+	/**
+	 * 设置当前状态
+	 **/
+	owner.setState = function(state) {
+		state = state || {};
+		localStorage.setItem('$state', JSON.stringify(state));
+		//var settings = owner.getSettings();
+		//settings.gestures = '';
+		//owner.setSettings(settings);
+	};
+
+	/**
+	 * 获取本地是否安装客户端
+	 **/
+	owner.isInstalled = function(id) {
+		if (id === 'qihoo' && mui.os.plus) {
+			return true;
+		}
+		if (mui.os.android) {
+			var main = plus.android.runtimeMainActivity();
+			var packageManager = main.getPackageManager();
+			var PackageManager = plus.android.importClass(packageManager)
+			var packageName = {
+				"qq": "com.tencent.mobileqq",
+				"weixin": "com.tencent.mm",
+				"sinaweibo": "com.sina.weibo"
+			}
+			try {
+				return packageManager.getPackageInfo(packageName[id], PackageManager.GET_ACTIVITIES);
+			} catch (e) {}
+		} else {
+			switch (id) {
+				case "qq":
+					var TencentOAuth = plus.ios.import("TencentOAuth");
+					return TencentOAuth.iphoneQQInstalled();
+				case "weixin":
+					var WXApi = plus.ios.import("WXApi");
+					return WXApi.isWXAppInstalled()
+				case "sinaweibo":
+					var SinaAPI = plus.ios.import("WeiboSDK");
+					return SinaAPI.isWeiboAppInstalled()
+				default:
+					break;
+			}
+		}
+	}
+}(mui, window.app = {}));
---
+++ InfoAPP/js/config.js
@@ -0,0 +1,128 @@
+//develop:
+//var posturl={
+//	login:'http://localhost:3000/login',
+//	photo:'http://localhost:3000/getphoto',
+//	photoupload:'http://localhost:3000/photoupload',
+//	note:'http://localhost:3000/note',
+//	edit:'http://localhost:3000/meg'
+//};
+var posturl={
+	login:'http://job.trueto.cn/login',
+	resume:'http://job.trueto.cn/resume',
+	photo:'http://job.trueto.cn/getphoto',
+	photoupload:'http://job.trueto.cn/photoupload',
+	note:'http://job.trueto.cn/note',
+	edit:'http://job.trueto.cn/meg'
+};
+//预加载提高页面切换时渲染效率，达到原生体验
+function openW (id,str,show) {
+	var chatPage = mui.preload({
+						"id": id,
+						"url": id+'.html'
+					});
+	return function() {
+			mui.fire(chatPage, 'show', null);
+			setTimeout(function() {
+				mui.toast(str);
+				mui.openWindow({
+					id: id,
+					show: {
+						aniShow: show
+					},
+					waiting: {
+						autoShow: false
+					}
+				});
+			}, 0);
+		}();
+}
+function getPhoto(page,limit,searchValue) {
+	$.ajax({
+		url:posturl.photo,
+		type:'POST',
+		dataType:'json',
+		data:{
+			page:page,
+			limit:limit,
+			searchValue:searchValue
+		},
+		success:function (data) {
+			localStorage.setItem('totalpage',data.totalPages);
+			var html='<ul id="hexGrid">';
+			$('#container').html('');
+			data.data.forEach(function (_data) {
+				html+='<li class="hex"><a class="hexIn" href="#"><img src='+_data.url+' data-preview-src="" data-preview-group="'+page+'" />'+
+	                    '<h1>'+_data.title+'</h1><p>'+_data.photodesc+'</p></a></li>';
+			});
+			html+='</ul>';
+			$('#container').html(html);
+		},
+		error:function () {
+			mui.toast('后台出错了！');
+		}
+	});
+}
+function getNote (page,limit,searchValue) {
+	$.ajax({
+		type:"post",
+		url:posturl.note,
+		dataType:'json',
+		async:false,
+		data:{
+			page:page,
+			limit:limit,
+			searchValue:searchValue
+		},
+		success:function (data) {
+			var table = document.body.querySelector('.mui-table-view');
+			localStorage.setItem('notePage',data.totalPages);
+			data.data.forEach(function (_data) {
+				var li = document.createElement('li');
+				li.className = 'mui-table-view-cell mui-media';
+				li.innerHTML = '<a id="'+_data.id+'" class="mui-navigate-right"><img class="mui-media-object mui-pull-left" src='+moodurl(_data.mood)+'>'+
+									'<div class="mui-media-body">'+_data.title+'<p class="mui-ellipsis">'+_data.date+'</p></div></a>';
+				table.appendChild(li);
+				detail(_data);
+			});
+		},
+		error:function () {
+			mui.toast('error');
+		}
+	});
+}
+function detail (data) {
+	var id=document.getElementById(data.id);
+	id.addEventListener('tap',function () {
+		mui.openWindow({
+			url:'detail.html',
+			id:'detail',
+			show:{
+				aniShow:'pop-in'
+			},
+			extras:{
+				data:data
+			}
+		});
+	});
+}
+function moodurl(i){
+	var m=0;
+	switch (i){
+		case 0:
+			m=14;
+			break;
+		case 1:
+			m=13;
+			break;
+		case 2:
+			m=1;
+			break;
+		case 3:
+			m=9;
+			break;
+		default:
+			m=3;
+			break;
+	}
+	return 'http://emoji.trueto.cn/'+m+'.gif';
+}
